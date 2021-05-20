d9b351a92732c11e987c5d206d26f25b00d7dbe0
TheKysek <thekysekozi@gmail.com>
2021-05-20 14:08:57 +0200

    weryfikacja schemy

--- js/script.js
+++
@@ -1,3 +0,0 @@
-// check schema is valid json
-// check data is valid json
-// validate data with json schema
---
+++ main.js
@@ -0,0 +1,36 @@
+const Ajv = require("ajv")
+const ajv = new Ajv({allErrors: true})
+
+global.testSchema = function() {
+    try {
+        var schema = JSON.parse($("#taSchema").val())
+    } catch (error) {
+        console.error(error);
+        $("#out").text("Błąd w składni schemy: " + error)
+        return
+    }
+    try {
+        var data = JSON.parse($("#taData").val())
+    } catch (error) {
+        console.error(error);
+        $("#out").text("Błąd w składni danych: " + error)
+        return
+    }
+
+
+    try {
+        var valid = ajv.validate(schema, data)
+    } catch (error) {
+        console.error(error);
+        $("#out").text("Błąd schemy: " + error)
+        return
+    }
+
+    if (valid) {
+        $("#out").text("Dane prawidłowe")
+    }
+    else {
+        $("#out").text("Dane nieprawidłowe: " + ajv.errorsText(valid.errors))
+    }
+}
+global.testSchema()
